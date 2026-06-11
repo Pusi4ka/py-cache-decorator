3b278d2784ba82fae5dbc65173ee86dbe1b7c1ab
Alex <klicker093@gmail.com>
2026-06-11 22:06:04 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -8,7 +8,6 @@ def cache(func: Callable) -> Callable:
 
     def wrapper(*args: Any, **kwargs: Any) -> Any:
         key = (func, args, tuple(sorted(kwargs.items())))
-        print(key)
         if key in store_data:
             print("Getting from cache")
             return store_data[key]
@@ -16,7 +15,6 @@ def cache(func: Callable) -> Callable:
         print("Calculating new result")
         result = func(*args, **kwargs)
         store_data[key] = result
-        print(store_data)
         return result
 
     return wrapper
